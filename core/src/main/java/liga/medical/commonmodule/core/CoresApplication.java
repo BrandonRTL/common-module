5b71e4a96bb81ef0af8f2831535aa82aafc08148
чсм чсм <faitneo@gmail.com>
2022-10-05 21:28:06 +0300

    initial

--- core/src/main/java/liga/medical/commonmodule/core/CoresApplication.java
+++ core/src/main/java/liga/medical/commonmodule/core/CoresApplication.java
@@ -1,6 +1,6 @@
 package liga.medical.commonmodule.core;
 
-public class MedicalMonitoringApplication {
+public class CoresApplication {
 
     public static void main(String[] args) {
         System.out.println("Я кор для либы.");
